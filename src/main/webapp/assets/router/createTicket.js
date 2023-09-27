555f62e5d66e1fb7b399222c21feaaa896f3bbde
Bhirahateesvaran Periyasamy <118425824+geek-bhirahatees@users.noreply.github.com>
2023-09-27 10:38:08 +0530

    Error handling add in ticket page

--- src/main/webapp/assets/router/createTicket.js
+++ src/main/webapp/assets/router/createTicket.js
@@ -1,6 +1,26 @@
 const form = document.querySelector("#ticket-raise");
 const from = sessionStorage.getItem("logginEmail");
 
+const sendEmail = async (data) => {
+	try {
+		const transporter = nodemailer.createTransport({
+			host: "smtp.office365.com",
+			port: 587,
+			secure: false, // Set to true if you're using a secure connection (TLS/SSL)
+			auth: {
+				user: "bhirahatees.periyasamy@outlook.com",
+				pass: "KQdpyG5Am5H7BBD",
+			},
+		});
+		const info = await transporter.sendMail(data);
+		console.log(info);
+		transporter.close();
+	} catch (err) {
+		console.error(err);
+	}
+
+}
+
 const userData = JSON.parse(sessionStorage.getItem("userData"));
 document.querySelector(".profile-logo").src = userData.profileImage;
 console.log(userData);
@@ -21,16 +41,35 @@ function createTicket(ticket) {
 }
 
 
+
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
 	const to = document.querySelector("#to_email").value;
 	const summary = document.querySelector("#summary").value;
 	const priority = document.querySelector("#priority").value;
 	const description = document.querySelector("#description").value;
-	const ticketData = {to,summary,priority,description}
-	try{
+	const ticketData = { to, summary, priority, description }
+	const emailTemplateString = `Hi ${userData.name}\n
+\n
+Thank you for reaching out to our support team. We have received your ticket and will get back to you as soon as possible.
+\n
+Here are the details of your ticket:
+\n
+Ticket description: ${description}\n
+\n
+We understand that your issue is important, and we will do our best to resolve it quickly. Our support team will be in touch with you shortly.\n
+\n
+If you have any additional information or updates regarding this ticket, please reply to this email and we'll update the ticket accordingly.\n
+\n
+Thank you for your patience.\n
+\n
+Best regards,\n
+Pupdesk`;
+
+	try {
 		createTicket(ticketData)
-	}catch(err){
+		sendEmail({ from: "bhirahatees.periyasamy@outlook.com", to: to, subject: summary, text: emailTemplateString });
+	} catch (err) {
 		console.log(err.message)
 	}
 })
